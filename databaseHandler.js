a3f9486e34a0407cf488761d087cee68dd335255
hiihihiiiiii <88477341+huongpham2001@users.noreply.github.com>
2021-12-31 03:14:39 +0700

    ok

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url =  "mongodb://huong17:phamthilanhuong200@cluster0-shard-00-00.wmaxf.mongodb.net:27017,cluster0-shard-00-01.wmaxf.mongodb.net:27017,cluster0-shard-00-02.wmaxf.mongodb.net:27017/test?replicaSet=atlas-iugmmi-shard-0&ssl=true&authSource=admin";
+const url =  "mongodb://huong17:phamthilanhuong2001@cluster0-shard-00-00.wmaxf.mongodb.net:27017,cluster0-shard-00-01.wmaxf.mongodb.net:27017,cluster0-shard-00-02.wmaxf.mongodb.net:27017/test?replicaSet=atlas-iugmmi-shard-0&ssl=true&authSource=admin";
 const dbName = "Test";
 
 async function getDbo() {
